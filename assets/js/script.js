6a555594d61d424689b9eb8f29681ece6e38e04a
Ryuggu <32564197+Ryuggu@users.noreply.github.com>
2018-03-08 14:00:59 +0100

    prepared for group work

---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+function getLocal()
+{
+    var notes = localStorage.getItem("noteList");
+
+    if (notes != null)
+    {
+        return JSON.parse(notes);
+    }
+    else
+    {
+        return [];
+    }
+}
+
+function setLocal(originNotes)
+{
+    var newNotes = JSON.stringify(originNotes);
+
+    localStorage.setItem("noteList", newNotes);
+}
+
+function submitNote(inputText, inputDate, inputImportant, inputIcon)
+{
+  var originNotes = getLocal();
+  var newNote = {
+    text:      inputText,
+    date:      inputDate,
+    important: inputImportant,
+    icon:      inputIcon
+  };
+
+  originNotes.push(newNote);
+
+  setLocal(originNotes);
+}
+
+function buildList()
+{
+  var notes = getLocal();
+
+  var ulElm = document.querySelector("ul");
+  ulElm.innerHTML = "";
+
+  for(var i = 0; i < notes.length; i++)
+  {
+    var liElm = document.createElement("li");
+    var pElm = document.createElement("p");
+
+    if(notes[i].important === true)
+    {
+      liElm.style.backgroundColor = "#800080";
+      liElm.style.color = "#FFFFFF";
+    }
+
+    pElm.innerHTML = notes[i].text;
+    pElm.classList.add("pinkUnicorn");
+
+    liElm.appendChild(pElm);
+
+    if(notes[i].date !== "")
+    {
+      var pDateElm = document.createElement("p");
+      pDateElm.innerHTML = notes[i].date;
+      liElm.appendChild(pDateElm);
+    }
+
+    ulElm.appendChild(liElm);
+  }
+}
+
+window.onload = function()
+{
+  buildList();
+}
+
+var submitBtn = document.querySelector("#addNote");
+
+    submitBtn.addEventListener("click", function()
+{
+  var text = document.querySelector("#noteText");
+  var important = document.querySelector("#noteImportant");
+  var date = document.querySelector("#noteTime");
+//  var icon = document.querySelector("#noteIcon");
+
+  submitNote(text.value, date.value, important.checked, "");
+  buildList();
+
+  text.value = "";
+  date.value = "";
+  important.checked = false;
+});
